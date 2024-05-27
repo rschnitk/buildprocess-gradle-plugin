fd98857b54a7cbad748116c084798948ae851bd6
Rainer Schnitker <R.Schnitker@ceyoniq.com>
2024-05-27 10:24:26 +0200

    fix missing import

--- src/main/java/io/github/rschnitk/buildprocess/UploadBomTask.java
+++ src/main/java/io/github/rschnitk/buildprocess/UploadBomTask.java
@@ -23,6 +23,7 @@ import org.gradle.api.file.RegularFileProperty;
 import org.gradle.api.provider.Property;
 import org.gradle.api.tasks.Input;
 import org.gradle.api.tasks.InputFile;
+import org.gradle.api.tasks.Optional;
 import org.gradle.api.tasks.TaskAction;
 
 /**
